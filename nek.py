3d712196bffb263ff7f3e314281ff28bd3aceb3d
Max Hutchinson <maxhutch@gmail.com>
2014-10-09 10:18:37 -0500

    Added script to copy nek output into a standard element ordering.

--- nek.py
+++ nek.py
@@ -1,10 +1,15 @@
 
 class NekFile():
-  def __init__(self, fname):
+  def __init__(self, fname, base = None):
+    # Do we have another file to base this off of?
+    if base != None:
+      self.init2(base, fname)
+      return
+
     import struct
     self.x_file = open(fname, 'rb')
-    header = str(self.x_file.read(132))
-    htoks = header.split()
+    self.header = self.x_file.read(132)
+    htoks = str(self.header).split()
     self.nelm = int(htoks[5])
     self.norder = int(htoks[2])
     self.time = float(htoks[7])
@@ -12,17 +17,41 @@ class NekFile():
     #''' Assume isotropic elements '''
     self.ntot = self.nelm * self.norder**3
     #''' Check the test float '''
-    test = struct.unpack('f', self.x_file.read(4))
-    byteswap = abs(test[0] - 6.543210029) > 0.00001
+    self.test = self.x_file.read(4)
+    self.test_tuple = struct.unpack('f', self.test)
+    byteswap = abs(self.test_tuple[0] - 6.543210029) > 0.00001
     if byteswap:
       print("  * swapping bytes")
       self.ty = '>f4'
     else:
       self.ty = 'f4'
+    self.bformat = None
+
     self.current_elm = 0
     self.u_file = open(fname, 'rb')
     self.p_file = open(fname, 'rb')
     self.t_file = open(fname, 'rb')
+    self.seek(0)
+
+  def init2(self, base, fname):
+    # copy state and write header
+    self.x_file = open(fname, 'wb')
+    self.header = base.header
+    self.x_file.write(self.header)
+    self.nelm = base.nelm
+    self.norder = base.norder
+    self.time = base.time
+    self.ntot = base.ntot
+    self.test = base.test
+    self.x_file.write(self.test)
+    self.ty = base.ty
+    self.bformat = base.bformat
+
+    self.u_file = open(fname, 'wb')
+    self.p_file = open(fname, 'wb')
+    self.t_file = open(fname, 'wb')
+
+    self.current_elm = 0
     self.seek(0)
 
   def close(self):
@@ -68,4 +97,34 @@ class NekFile():
     self.current_elm += num
 
     return num, x, u, p, t
+
+  def write(self, x, u, p, t, ielm = -1):
+
+    # create a binary formatter
+    if self.bformat == None:
+      import struct
+      if self.ty == '>f4':
+        fmt = '{:d}>f'.format(self.norder**3)
+      else:
+        fmt = '{:d}f'.format(self.norder**3)
+      self.bformat = struct.Struct(fmt)
+
+    # seek where we need to be
+    if ielm >= 0:
+      self.seek(ielm)
+
+    # write a single element's worth of data using binary formatter
+    import numpy as np
+    self.x_file.write(self.bformat.pack(*(x[:,0,0].astype(np.float32).tolist())))
+    self.x_file.write(self.bformat.pack(*(x[:,0,1].astype(np.float32).tolist())))
+    self.x_file.write(self.bformat.pack(*(x[:,0,2].astype(np.float32).tolist())))
+
+    self.u_file.write(self.bformat.pack(*(u[:,0,0].astype(np.float32).tolist())))
+    self.u_file.write(self.bformat.pack(*(u[:,0,1].astype(np.float32).tolist())))
+    self.u_file.write(self.bformat.pack(*(u[:,0,2].astype(np.float32).tolist())))
+
+    self.p_file.write(self.bformat.pack(*(p[:,0].astype(np.float32).tolist())))
+    self.t_file.write(self.bformat.pack(*(t[:,0].astype(np.float32).tolist())))
+
+    return
 
